20156c9293899abbe243047549ede96f55f8b90b
Regina <tuturiw@mail.ru>
2023-12-11 13:31:12 +0300

    styles are distributed in folders

--- src/components/FilmListDetail.tsx
+++ src/components/FilmListDetail.tsx
@@ -11,7 +11,7 @@ const FilmDetail: FC<FilmDetailProps> = ({film}) => {
         <a href="#">
             <div className="filmDetail">
                 <img className="filmDetail_img" src="public/images/test-film.png" alt="film image"/>
-                {/*<img className="filmDetail_img" src={props.url} alt="film image"/>*/}
+                {/*<img className="filmDetail_img" src={film.url} alt="film image"/>*/}
                 <span className="filmDetail_text">{film.title}</span>
             </div>
         </a>
